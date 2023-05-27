c9f044f687fc12976786ba3e2c72d362611f2446
hachem <hb@big-mama.io>
2023-05-28 00:15:14 +0100

    add transcribers

---
+++ backend/src/transcribe.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+from youtube_transcript_api import YouTubeTranscriptApi
+import whisper
+
+
+class Transcriber(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def transcribe(url: str) -> str:
+        """turn video into text"""
+
+
+class Whisper(Transcriber):
+    def __init__(self, model_name: str, **kwargs):
+        super().__init__(**kwargs)
+        self.model = whisper.load_model(model_name)
+
+    def transcribe(self, path: str) -> str:
+        transcription = self.model.transcribe(path)
+        transcription = transcription["text"]
+        return transcription
+
+
+class YoutubeTranscriber(Transcriber):
+    def __init__(self, **kwargs: any):
+        super().__init__(**kwargs)
+
+    def transcribe(self, video_id: str) -> str:
+        try:
+            transcript = YouTubeTranscriptApi.get_transcript(video_id)
+            transcript = [t["text"] for t in transcript]
+        except:
+            transcript = ""
+
+        return " ".join(transcript)

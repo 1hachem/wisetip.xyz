7df727679483407e040580541be66a731d486c94
hachem <hb@big-mama.io>
2023-06-03 19:42:58 +0100

    init api

---
+++ mailist/src/pages/api/[email].ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from "@prisma/client";
+import { NextApiRequest, NextApiResponse } from "next";
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { email } = req.query;
+  const request = await prisma.email
+    .create({
+      data: {
+        email: email,
+      },
+    })
+    .then(async () => {
+      await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+      console.error(e);
+      await prisma.$disconnect();
+      process.exit(1);
+    });
+}

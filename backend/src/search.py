a6a5087902a9c064b10fa606a4e001b1e7ad9a51
hachem <hb@big-mama.io>
2023-06-09 20:59:48 +0100

    update search

--- backend/src/search.py
+++ backend/src/search.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python
 
-# This sample executes a search request for the specified search term.
-# Sample usage:
-#   python search.py --q=surfing --max-results=10
-# NOTE: To use the sample, you must provide a developer key obtained
-#       in the Google APIs Console. Search for "REPLACE_ME" in this code
-#       to find the correct place to provide that key..
 import argparse
 import os
 
@@ -16,30 +10,22 @@ from googleapiclient.errors import HttpError
 dotenv.load_dotenv()
 
 
-# Set DEVELOPER_KEY to the API key value from the APIs & auth > Registered apps
-# tab of
-#   https://cloud.google.com/console
-# Please ensure that you have enabled the YouTube Data API for your project.
 DEVELOPER_KEY = os.getenv("YOUTUBE_API_KEY")
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
 
 def youtube_search(query: str, max_results: int = 5) -> list[str]:
+    """return a list of video ids"""
     youtube = build(
         YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY
     )
 
-    # Call the search.list method to retrieve results matching the specified
-    # query term.
     search_response = (
         youtube.search().list(q=query, part="id", maxResults=max_results).execute()
     )
 
     videos = []
-
-    # Add each result to the appropriate list, and then display the lists of
-    # matching videos, channels, and playlists.
     for search_result in search_response.get("items", []):
         if search_result["id"]["kind"] == "youtube#video":
             videos.append(search_result["id"]["videoId"])
@@ -47,7 +33,11 @@ def youtube_search(query: str, max_results: int = 5) -> list[str]:
     return videos
 
 
-if __name__ == "__main__":
+def youtube_search_product(product: str, max_results: int = 5) -> list[str]:
+    """returns top videos urls for how to choose the right [product]"""
+    query = f"how to choose the right {product}"
+    top_videos = youtube_search(query, max_results=max_results)
+    return top_videos
     parser = argparse.ArgumentParser()
     parser.add_argument("--query", help="Search term")
     parser.add_argument("--max-results", help="Max results", default=5)

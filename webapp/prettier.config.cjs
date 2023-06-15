65eb9e78b452e3df030c145e579a40fb9624129d
Ghribi Ouassim Abdelmalek <oag@big-mama.io>
2023-06-15 12:59:17 +0100

    added prettier with tailwindcss for class format

--- webapp/prettier.config.cjs
+++ webapp/prettier.config.cjs
@@ -1,5 +1,15 @@
 /** @type {import("prettier").Config} */
 const config = {
+  jsxSingleQuote: true,
+  singleQuote: true,
+  semi: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  printWidth: 100,
+  bracketSameLine: false,
+  useTabs: false,
+  arrowParens: "always",
+  endOfLine: "auto",
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
 };
 
